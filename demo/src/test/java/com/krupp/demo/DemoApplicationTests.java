1b5040174303333c3c12c98eb3cfdd789e05a957
liuguangzheng <rrefe4@163.com>
2019-09-05 17:25:26 +0800

    Spring中Bean的生命周期

---
+++ demo/src/test/java/com/krupp/demo/DemoApplicationTests.java
@@ -0,0 +1,30 @@
+package com.krupp.demo;
+
+import com.krupp.demo.ioc.Person;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("现在开始初始化容器");
+
+        ApplicationContext factory = new ClassPathXmlApplicationContext(
+                "/beans.xml");
+        System.out.println("容器初始化成功");
+        // 得到Preson，并使用
+        Person person = factory.getBean("person", Person.class);
+        System.out.println(person);
+
+        System.out.println("现在开始关闭容器！");
+        ((ClassPathXmlApplicationContext) factory).registerShutdownHook();
+    }
+}
+
