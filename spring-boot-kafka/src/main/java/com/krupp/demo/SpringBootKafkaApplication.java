a1f3e5edc452fe55e137ec979dad3c9a71cec593
刘光正 <rrefe4@163.com>
2019-12-17 17:33:38 +0800

    spring-boot整合kafka

--- spring-boot-kafka/src/main/java/com/krupp/demo/SpringBootKafkaApplication.java
+++ spring-boot-kafka/src/main/java/com/krupp/demo/SpringBootKafkaApplication.java
@@ -1,10 +1,16 @@
 package com.krupp.demo;
 
+import com.krupp.demo.producer.UserLogProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootKafkaApplication {
+
+    @Autowired
+    private UserLogProducer kafkaSender;
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootKafkaApplication.class, args);
--- spring-boot-kafka/src/test/java/com/krupp/demo/SpringBootKafkaApplicationTests.java
+++ spring-boot-kafka/src/test/java/com/krupp/demo/SpringBootKafkaApplicationTests.java
@@ -1,10 +1,22 @@
 package com.krupp.demo;
 
+import com.krupp.demo.producer.UserLogProducer;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootKafkaApplicationTests {
+    @Autowired
+    private UserLogProducer kafkaSender;
+
+    @Test
+    public void init(){
+        for (int i = 0; i < 10; i++) {
+            //调用消息发送类中的消息发送方法
+            kafkaSender.sendLog(String.valueOf(i));
+        }
+    }
 
     @Test
     void contextLoads() {
