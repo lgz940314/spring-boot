c495d6d1863bb4223c27583d912c48111571547e
刘光正 <rrefe4@163.com>
2019-12-17 14:59:01 +0800

    spring-boot整合kafka

---
+++ spring-boot-kafka/src/test/java/com/krupp/demo/SpringBootKafkaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.krupp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
